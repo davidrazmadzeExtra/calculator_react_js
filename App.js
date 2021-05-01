595e54094b0d1e25ea061064db07ff75ea0674a6
David Razmadze <75696759+davidrazmadzeExtra@users.noreply.github.com>
2021-04-30 23:21:05 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import * as math from "mathjs";
+
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + " "]);
+  };
+
+  const calculateResult = () => {
+    const input = text.join(""); // Remove commas
+
+    setResult(math.evaluate(input));
+  };
+
+  const resetInput = () => {
+    setText("");
+    setResult("");
+  };
+
+  const buttonColor = "#f2a33c";
+
+  return (
+    <div className="App">
+      <div className="calc-wrapper">
+        <Input text={text} result={result} />
+        <div className="row">
+          <Button symbol="7" handleClick={addToText} />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
+          <Button symbol="/" color={buttonColor} handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
+          <Button symbol="*" color={buttonColor} handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
+          <Button symbol="+" color={buttonColor} handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol="0" handleClick={addToText} />
+          <Button symbol="." handleClick={addToText} />
+          <Button symbol="=" handleClick={calculateResult} />
+          <Button symbol="-" color={buttonColor} handleClick={addToText} />
+        </div>
+        <Button symbol="Clear" color="red" handleClick={resetInput} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Input.js
@@ -0,0 +1,17 @@
+import "./Input.css";
+
+const Input = ({ text, result }) => {
+  return (
+    <div className="input-wrapper">
+      <div className="result">
+        <h1>{result}</h1>
+      </div>
+
+      <div className="text">
+        <h3>{text}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
